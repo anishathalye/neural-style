b325725194ac232b50e4744098b726a39062ed4f
Anish Athalye <me@anishathalye.com>
2017-02-18 18:10:09 -0500

    Update deprecated function

--- stylize.py
+++ stylize.py
@@ -110,7 +110,7 @@ def stylize(network, initial, content, styles, iterations,
         best_loss = float('inf')
         best = None
         with tf.Session() as sess:
-            sess.run(tf.initialize_all_variables())
+            sess.run(tf.global_variables_initializer())
             for i in range(iterations):
                 last_step = (i == iterations - 1)
                 print_progress(i, last=last_step)
